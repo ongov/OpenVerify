18de792b1809a81f6180daa73e42a83e64388cec
David Manouchehri <david.manouchehri@riseup.net>
2021-11-04 14:23:45 -0400

    Fix incorrect string matching.
    Using includes is incorrect, as it would match things like `vaccine-ontario.ca.totally.different.randomdomain.tk`.

--- src/services/QRCodeValidator/QRCodeValidator.ts
+++ src/services/QRCodeValidator/QRCodeValidator.ts
@@ -121,7 +121,7 @@ export default class QRCodeValidator {
       }
       return {
         valid: false,
-        thirdParty: input.includes('vaccine-ontario.ca'),
+        thirdParty: input.endsWith('vaccine-ontario.ca'),
         multi:
           this.#totalChunks !== undefined
             ? {
